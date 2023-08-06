7efd6bfe683433f657ffdd9367d13f9228144697
yoquer <123332655+yoquer@users.noreply.github.com>
2023-08-06 17:28:00 -0400

    Delete src directory

--- src/paths.ts
+++
@@ -1,9 +0,0 @@
-import 'module-alias/register';
-import { addAliases  } from "module-alias"; 
-
-addAliases({
-    "@base": __dirname + "/libs/structure/base",
-	"@Client": __dirname + "/libs/structure/client/client",
-	"@clients": __dirname + "/libs/structure/client",
-	"@Config": __dirname + "/assets/config"
-})
